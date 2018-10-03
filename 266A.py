d447955e75b9db85a83f2530e251d97cdc74fcd7
abhinavdv <dvabhinav31@gmail.com>
2018-10-03 21:13:44 +0530

    Update 266A.py

--- 266A.py
+++ 266A.py
@@ -2,7 +2,7 @@ n=int(input())
 l=input()
 c=0
 for i in range(n-1):
-    if l[i]+l[i+1]=="RR" or l[i]+l[i+1]=="BB" or l[i]+l[i+1]=="GG":
+    if l[i]+l[i+1]=="RR" or l[i]+l[i+1]=="BB" or l[i]+l[i+1]=="GG":      #cvfdgdfg
         c+=1
 print(c)        
         
